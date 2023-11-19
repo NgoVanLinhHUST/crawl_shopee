3d2ba4bb46466eed06364f45739d684a56a1e193
Linh <nvlinh.hust@gmail.com>
2023-11-20 00:09:07 +0700

    update setting

--- crawl_shopee/settings.py
+++ crawl_shopee/settings.py
@@ -63,9 +63,9 @@ COOKIES_ENABLED = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-# ITEM_PIPELINES = {
-#    "crawl_shopee.pipelines.CrawlHoteVntripPipeline": 400,
-# }
+ITEM_PIPELINES = {
+   "crawl_shopee.pipelines.CrawlShopee": 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
